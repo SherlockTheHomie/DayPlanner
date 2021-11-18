9885a4d45e89f6f4104e3e1238034a1749c99cea
Gabe L'Herault <glherault@tcmsolutions.net>
2021-11-18 15:32:58 -0600

    Trimming code fat because this chongus needs lipo

--- assets/logic.js
+++ assets/logic.js
@@ -3,6 +3,10 @@ setInterval(function() {
     $("#currentDay").text(today.format("dddd, MMMM Do"));
 }, 1000);
 
+isItTime();
+
+let scheduleStorage = [];
+
 let nine = document.getElementById("nine");
 nine.time = 9;
 let ten = document.getElementById("ten");
@@ -24,8 +28,17 @@ fifteen= 17;
 
 let schedTime;
 
+let scheduleEvent = document.this.form("");
+
+document.forms.addEventListener("submit", storageSubmit);
+
+function storageSubmit(scheduleEvent) {
+    let clicked = scheduleEvent.currentTarget;   
+}
 
 let timeArray = [nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen];
+
+let scheduleEvent = document.this.form("");
 
 
 function isItTime() {
@@ -44,7 +57,16 @@ function isItTime() {
     }        
 };
 
-let scheduleStorage = [] || JSON.parse(window.localStorage.getItem("scheduleStorage"));
+function pageLoad() {
+    if (localStorage. getItem("scheduleStorage") === null) {
+    localStorage.setItem("scheduleStorage", "");
+    // } else if (localStorage !== null) {
+    //     JSON.parse(window.localStorage.getItem("scheduleStorage"));
+    // }
+    // return ;
+}
+
+
 
 let nineSubmit = document.getElementById("nine-am");
 
@@ -54,9 +76,32 @@ nineSubmit.addEventListener("submit", function(event) {
     let scheduleEvent = {
         description: nineInput.value,
         time: nine.time
-    }
-    localStorage.setItem('scheduleStorage', JSON.stringify(scheduleEvent));
+    }   
+    localStorage.setItem(nine.time, JSON.stringify(scheduleEvent));
 });
+
+
+
+
+
+// var testObject = [{ 'name': 'James', 'score': 90, 'time': '16:00' }, { 
+//     'name': 'Robert', 'score': 80, 'time': '15:00' }];
+//     localStorage.setItem('testObject', JSON.stringify(testObject));
+    
+//     var retrievedObject = JSON.parse(localStorage.getItem('testObject'));
+    
+//     var tbody = document.getElementById('tbody');
+    
+//     for (var i = 0; i < retrievedObject.length; i++) {
+//       var tr = "<tr>";
+//       tr += "<td>Name</td>" + "<td>" + retrievedObject[i].name + "</td></tr>";
+//       tr += "<td>Score</td>" + "<td>" + retrievedObject[i].score + "</td></tr>";
+//       tr += "<td>Time</td>" + "<td>" + retrievedObject[i].time + "</td></tr>";
+//       tbody.innerHTML += tr;
+//     }
+
+// localStorage.getItem(eventStorage)
+
 
 
 console.log(moment().hours());
