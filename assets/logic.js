2be43a41357f2f527d33c985b66c86b9e16e7285
Gabe L'Herault <glherault@tcmsolutions.net>
2021-11-28 14:57:40 -0600

    I scrapped my HTML and rebuilt it utilizing the CSS and changed the logic to work

--- assets/logic.js
+++ assets/logic.js
@@ -1,21 +1,10 @@
-setInterval(function() {
+setInterval(function () {
     var today = moment();
     $("#currentDay").text(today.format("dddd, MMMM Do"));
 }, 1000);
 
 
 
-function pageLoad() {
-    if (localStorage. getItem("scheduleStorage") === null) {
-    localStorage.setItem("scheduleStorage", "");
-} else if (localStorage !== null) {
-  JSON.parse(window.localStorage.getItem("scheduleStorage"));
- }
- return ;
-}
-
-
-let scheduleStorage = [];
 
 let nine = document.getElementById("nine");
 nine.time = 9;
@@ -26,61 +15,64 @@ eleven.time = 11;
 let twelve = document.getElementById("twelve");
 twelve.time = 12;
 let thirteen = document.getElementById("thirteen");
-thirteen = 13;
+thirteen.time = 13;
 let fourteen = document.getElementById("fourteen");
-fourteen= 14;
+fourteen.time = 14;
 let fifteen = document.getElementById("fifteen");
-fifteen= 15;
+fifteen.time = 15;
 let sixteen = document.getElementById("sixteen");
-fifteen= 16;
+sixteen.time = 16;
 let seventeen = document.getElementById("seventeen");
-fifteen= 17;
-
-let schedTime;
-
-// let scheduleEvent = document.this.form(input.value);
+seventeen.time = 17;
 
 
+let forms = document.querySelectorAll("form");
+console.log(forms);
+
+saveBtn = document.querySelectorAll("#saveBtn");
+
+forms.forEach(form => {
+    form.children[1].value = localStorage.getItem(form.id) ? localStorage.getItem(form.id): "";
+})    
 
 
 
-// Make object of form input value
+saveBtn.forEach(form => {
+    form.addEventListener("click", function (e) {
+        e.preventDefault();
+        forms.forEach(form => {
+            let scheduleEvent = form.children[1].value;
+            // console.log(scheduleEvent);
+            // let hour = e.target.id;
+            // console.log(hour);
+            // let inputsObject = {
+            //     time: hour,
+            //     toDo: scheduleEvent
+            // }    
+            // scheduleStorage.push(inputsObject);
+            localStorage.setItem(form.id, scheduleEvent);
+        })    
+})        
+});
 
-document.forms.addEventListener("submit", function(e) {
-    preventDefault(e);
-    let clicked = scheduleEvent.currentTarget;
-    clicked.id = [input.value];
-    localStorage.setItem("scheduleStorage", [scheduleEvent]);
-    }
-);
-
-// function storageSubmit() {
-//     // get the dom object out of event
-//     // get input value from form.
-//     // add to schedule object
-//     // add get item from storage to pageload
-//     localStorage.setItem("schedule", JSON.stringify(scheduleEvent));
-// }
-
-let timeArray = [nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen];
+let hoursArray = [nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen];
 
 // let scheduleEvent = document.this.form("");
+let schedTime;
 
+isItTime();
 
 function isItTime() {
-   
-    for (let i = 0; i < timeArray.length; i++) {
-        schedTime = timeArray[i].time;
+    for (let i = 0; i < hoursArray.length; i++) {
+        schedTime = hoursArray[i].time;
         if (moment().hours() === schedTime) {
-            timeArray[i].setAttribute("style", "background-color: red");
+            hoursArray[i].setAttribute("style", "background-color: red");
+        } else if (moment().hours() < schedTime) {
+            hoursArray[i].setAttribute("style", "background-color: green");
+        } else if (moment().hours() > schedTime) {
+            hoursArray[i].setAttribute("style", "background-color: grey");
         }
-        else if (moment().hours() > schedTime) {
-            timeArray[i].setAttribute("style", "background-color: green");
-        }
-        else if (moment().hours() < schedTime) {
-            timeArray[i].setAttribute("style", "background-color: grey");
-        }  
-    }        
-};     
+    }
+}
 
-  console.log(moment().hours());
+console.log(moment().hours());
